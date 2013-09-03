e1032eef63ef521b4f02978df7f201b71983f553
Martin Olveyra <molveyra@gmail.com>
2013-09-03 15:12:15 -0200

    added new test case and fix. Also added more docstring and changed method name

--- sequential_parser.py
+++ sequential_parser.py
@@ -10,13 +10,17 @@ from scrapely.htmlpage import HtmlPage, HtmlTag
 def raw_to_text(txt):
     return _text(htmlregion(txt))
 
-def _match_key(candidate, compiled_keys_map):
-    if isinstance(candidate, basestring):
+def _match_state(text, compiled_keys_map):
+    """
+    Check whether the given text matches some of the state ids
+    Returns back the complete text, and the substring to be appended to data extracted
+    """
+    if isinstance(text, basestring):
         for key, regex in compiled_keys_map.items():
-            m = regex.search(candidate)
+            m = regex.search(text)
             if m:
                 return key, m.groups()[0] if m.groups() else None
-    return candidate, None
+    return text, None
 
 def sequential_parse(data, sections, encoding="utf-8", debug=False):
     """
@@ -25,7 +29,7 @@ def sequential_parse(data, sections, encoding="utf-8", debug=False):
       Otherwise extraction starts after the jump.
       numeric state ids are useful for avoiding unexpected matches (to ensure that state is reached only by manual jump)
     - section values are binary tuples. first element is the field name switched to by the state. Can be None in order to
-      avoid any further extraction until state is changed.
+      avoid any further extraction until next state change.
     - second element is jump state id.
                 * If None, conserves state until an automatic (text matching)
                   state switch is performed.
@@ -73,6 +77,24 @@ def sequential_parse(data, sections, encoding="utf-8", debug=False):
     >>> item['hello_field_2']
     [u'hello data 2']
 
+    and if a state with field name None is reached, extraction will be skipped until next state switch
+    >>> sections = {"hello header": (None, "1"), "1":("hello_field_2", None), "bye header": ("bye_field", None)}
+    >>> item = sequential_parse(data, sections)[0]
+    >>> sorted(item.keys())
+    ['bye_field', 'hello_field_2']
+    >>> item['bye_field']
+    [u'bye data']
+    >>> item['hello_field_2']
+    [u'hello data 2']
+
+    A simple example with field name None
+    >>> sections = {"hello header": ("hello_field", None), "bye header": (None, None)}
+    >>> item = sequential_parse(data, sections)[0]
+    >>> item.keys()
+    ['hello_field']
+    >>> item["hello_field"]
+    [u'hello data', u'hello data 2']
+
     using a not existing state id as jump target will finish current subitem and create a new one after the first extraction
     >>> sections = {"hello header": ("hello_field", 1), "bye header": ("bye_field", None)}
     >>> item, item2 = sequential_parse(data, sections)
@@ -108,7 +130,7 @@ def sequential_parse(data, sections, encoding="utf-8", debug=False):
     >>> item['hello_field']
     [u'hello data']
 
-    or target field is None. In this case, creation of new item is immediate
+    or target field is None. In this case, creation of new item is immediate (because target state does not exist)
     >>> sections = {"hello header": ("hello_field", None), "hello data 2": (None, 1), "bye header": ("bye_field", None)}
     >>> item, item2 = sequential_parse(data, sections)
     >>> sorted(item.keys())
@@ -120,17 +142,8 @@ def sequential_parse(data, sections, encoding="utf-8", debug=False):
     >>> item2['bye_field']
     [u'bye data']
 
-
-    When a state with field name None is reached, extraction will be skipped until next state switch
-    >>> sections = {"hello header": ("hello_field", None), "bye header": (None, None)}
-    >>> item = sequential_parse(data, sections)[0]
-    >>> item.keys()
-    ['hello_field']
-    >>> item["hello_field"]
-    [u'hello data', u'hello data 2']
-   
     The initial state is None, which by default is setted to (None, None), so will not start to extract nothing until
-    first text match is found.
+    first text match is found. This default behavior can be overriden by adding an explicit None state.
     >>> sections = {None: ("hello_field", None), "bye header": (None, None)}
     >>> item = sequential_parse(data, sections)[0]
     >>> item.keys()
@@ -171,16 +184,15 @@ def sequential_parse(data, sections, encoding="utf-8", debug=False):
     for e in page.parsed_body:
         text = page.body[e.start:e.end].strip()
         if not isinstance(e, HtmlTag) and e.is_text_content and text:
-            key, append = _match_key(text, compiled_keys)
+            key, append = _match_state(text, compiled_keys)
             if key in sections:
                 current_field, jump = _switch(key)
                 if debug:
                     print "%s --> %s" % (key, jump)
-                # print "'%s' '%s' '%s' '%s'" % (key, current_field, jump, append)
                 if append:
                     _set_field(item_data, current_field, append)
                     if jump is not None:
-                        jump, _ = _match_key(jump, compiled_keys)
+                        jump, _ = _match_state(jump, compiled_keys)
                         if jump in sections:
                             current_field, jump = _switch(jump)
                         else:
@@ -201,7 +213,7 @@ def sequential_parse(data, sections, encoding="utf-8", debug=False):
             elif current_field is not None:
                 _set_field(item_data, current_field, text)
                 if jump is not None:
-                    jump, _ = _match_key(jump, compiled_keys)
+                    jump, _ = _match_state(jump, compiled_keys)
                     if jump in sections:
                         current_field, jump = _switch(jump)
                     else:
@@ -211,6 +223,9 @@ def sequential_parse(data, sections, encoding="utf-8", debug=False):
                             return subitems
                         item_data = {}
                         current_field, jump = _switch(None)
+            else:
+                current_field, jump = _switch(jump)
+
     else:
         if item_data:
             subitems.append(item_data)
